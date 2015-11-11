7fd33fad1f3d4e5654e398d7d67e335ef8b254b2
nicolaihald <haldcph@gmail.com>
2015-11-11 11:29:33 +0100

    Fixed stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-isExistApp=`pgrep php5-fpm`
-if [[ -n  \$isExistApp ]]; then
-    service php5-fpm stop
+SERVICE='php5-fpm'
+if P=$(pgrep $SERVICE)
+then
+    echo "$SERVICE is running, PID is $P. Stopping service..."
+	service php5-fpm stop
+else
+    echo "$SERVICE is not running"
 fi
-isExistApp=`pgrep nginx`
-if [[ -n  \$isExistApp ]]; then
-   service nginx stop
+
+SERVICE='nginx'
+if P=$(pgrep $SERVICE)
+then
+    echo "$SERVICE is running, PID is $P. Stopping service..."
+	service nginx stop
+else
+    echo "$SERVICE is not running"
 fi
